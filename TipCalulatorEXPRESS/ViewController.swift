d6eb4298524df39db2d063098e1eaa9277828723
Michael Benton <hydromac@mikes-mac.local>
2019-01-23 21:12:35 -0800

    Initial Commit

---
+++ TipCalulatorEXPRESS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalulatorEXPRESS
+//
+//  Created by Mike Benton on 1/23/19.
+//  Copyright © 2019 Mike Benton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
